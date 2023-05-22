2e370be5a13262568281b765a01ab775f4a37a99
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-05-22 11:18:56 +0700

    golang database migration

--- app/db.go
+++ app/db.go
@@ -8,11 +8,20 @@ import (
 )
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:@root123@tcp(localhost:3306)/belajar_golang_result_api")
+	db, err := sql.Open("mysql", "root:@root123@tcp(localhost:3306)/belajar_golang_database_migration")
 	helper.PanicIfError(err)
 	db.SetMaxIdleConns(5)
 	db.SetMaxOpenConns(20)
 	db.SetConnMaxLifetime(60 * time.Minute)
 	db.SetConnMaxIdleTime(10 * time.Minute)
 	return db
+
+	// migrate create -ext sql -dir db/migration create_first
+
+	// migrate -database "mysql://root:@root123@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migration up
+
+	// migrate -database "mysql://root:@root123@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migration down
+	// migrate -database "mysql://root:@root123@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migration version
+	// migrate -database "mysql://root:@root123@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migration force version before dirty
+
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module github.com/juandaantoniusapakpahan/go-restful-api
 go 1.20
 
 require (
+	github.com/go-playground/validator/v10 v10.13.0
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/julienschmidt/httprouter v1.3.0
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible // indirect
-	github.com/go-playground/validator/v10 v10.13.0 // indirect
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/leodido/go-urn v1.2.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
